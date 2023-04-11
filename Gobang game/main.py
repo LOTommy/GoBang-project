694437dc5bad146fc892ed8159eb304687dfd4fd
pm-ep <105105579+pm-ep@users.noreply.github.com>
2023-04-11 23:54:19 +0800

    Updated with 2 options

--- Gobang game/main.py
+++ Gobang game/main.py
@@ -1,9 +1,10 @@
 from classes.GameBoard import GameBoard
-from classes.Player import Player
-import random
 
 if __name__ == '__main__':
     name1="Apple"
     name2="Banana"
+    #For PvP
     Board=GameBoard(name1,name2)
+    #For PvC
+    #Board=GameBoard(name1)
     Board.run()
