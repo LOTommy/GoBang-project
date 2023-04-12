b7f4e6091e7c3ba08d5e69bd8c559dc555d36d50
pm-ep <105105579+pm-ep@users.noreply.github.com>
2023-04-13 00:05:49 +0800

    Remove unused properties

--- classes/Square.py
+++ classes/Square.py
@@ -3,12 +3,11 @@ from .Piece import Piece
 
 WIDTH_MARGIN=300
 HEIGHT_MARGIN=100
-BOARD_SIZE=19
-SQ_WIDTH=30
-SQ_HEIGHT=30
+SQ_WIDTH=40
+SQ_HEIGHT=40
 
 class Square:
-    def __init__(self, x, y,piece):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.width = SQ_WIDTH
@@ -16,8 +15,7 @@ class Square:
         self.screen_x = x * self.width + WIDTH_MARGIN
         self.screen_y = y * self.height + HEIGHT_MARGIN
         self.actual_coord = (self.screen_x, self.screen_y)
-        self.colour = (0, 0, 0)
-        self.occupying_piece = Piece(x,y,piece)
+        self.colour = (164, 55, 0)
         self.rect = pg.Rect(
             self.screen_x,
             self.screen_y,
@@ -27,5 +25,3 @@ class Square:
 
     def draw(self, window):
         pg.draw.rect(window, self.colour, self.rect,1)
-
-        self.occupying_piece.draw(window)
