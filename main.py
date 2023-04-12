a6fb22ec28e6421dad15cf3f473f962b34c27357
pm-ep <105105579+pm-ep@users.noreply.github.com>
2023-04-13 00:15:39 +0800

    Update using pygbag

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import asyncio
 from classes.GameBoard import GameBoard
 
-if __name__ == '__main__':
-    name1="Apple"
-    name2="Banana"
-    
-    #For PvP
-    Board=GameBoard(name1,name2)
-    #For PvC
-    #Board=GameBoard(name1)
+COUNT_DOWN = 3
 
-    Board.run()
+async def main():
+    global COUNT_DOWN
+
+    COUNT_DOWN = 0
+
+    while True:
+
+        name1="Apple"
+        name2="Banana"
+        
+        #For PvP
+        Board=GameBoard(name1,name2)
+        #For PvC
+        #Board=GameBoard(name1)
+
+        Board.run()
+
+        await asyncio.sleep(0)  # Very important, and keep it 0
+
+        if not COUNT_DOWN:
+            return
+
+        COUNT_DOWN = COUNT_DOWN - 1
+
+asyncio.run(main())
